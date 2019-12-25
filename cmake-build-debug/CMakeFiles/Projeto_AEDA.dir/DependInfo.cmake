ccf1ac109227ac134e53c43a951666519365a363
caionoficial <caion00@hotmail.com>
2019-12-25 19:13:58 +0000

    BST implementada

---
+++ cmake-build-debug/CMakeFiles/Projeto_AEDA.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/AEDA/repos/1-Projeto_AEDA/Empresa.cpp" "F:/AEDA/repos/1-Projeto_AEDA/cmake-build-debug/CMakeFiles/Projeto_AEDA.dir/Empresa.cpp.obj"
+  "F:/AEDA/repos/1-Projeto_AEDA/Person.cpp" "F:/AEDA/repos/1-Projeto_AEDA/cmake-build-debug/CMakeFiles/Projeto_AEDA.dir/Person.cpp.obj"
+  "F:/AEDA/repos/1-Projeto_AEDA/ServicoTransporte.cpp" "F:/AEDA/repos/1-Projeto_AEDA/cmake-build-debug/CMakeFiles/Projeto_AEDA.dir/ServicoTransporte.cpp.obj"
+  "F:/AEDA/repos/1-Projeto_AEDA/Utils.cpp" "F:/AEDA/repos/1-Projeto_AEDA/cmake-build-debug/CMakeFiles/Projeto_AEDA.dir/Utils.cpp.obj"
+  "F:/AEDA/repos/1-Projeto_AEDA/camiao.cpp" "F:/AEDA/repos/1-Projeto_AEDA/cmake-build-debug/CMakeFiles/Projeto_AEDA.dir/camiao.cpp.obj"
+  "F:/AEDA/repos/1-Projeto_AEDA/main.cpp" "F:/AEDA/repos/1-Projeto_AEDA/cmake-build-debug/CMakeFiles/Projeto_AEDA.dir/main.cpp.obj"
+  "F:/AEDA/repos/1-Projeto_AEDA/menus.cpp" "F:/AEDA/repos/1-Projeto_AEDA/cmake-build-debug/CMakeFiles/Projeto_AEDA.dir/menus.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Projeto_AEDA.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projeto_AEDA.dir/Empresa.cpp.obj"
+  "CMakeFiles/Projeto_AEDA.dir/Person.cpp.obj"
+  "CMakeFiles/Projeto_AEDA.dir/ServicoTransporte.cpp.obj"
+  "CMakeFiles/Projeto_AEDA.dir/Utils.cpp.obj"
+  "CMakeFiles/Projeto_AEDA.dir/camiao.cpp.obj"
+  "CMakeFiles/Projeto_AEDA.dir/main.cpp.obj"
+  "CMakeFiles/Projeto_AEDA.dir/menus.cpp.obj"
+  "Projeto_AEDA.exe"
+  "Projeto_AEDA.exe.manifest"
+  "Projeto_AEDA.pdb"
+  "libProjeto_AEDA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projeto_AEDA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
